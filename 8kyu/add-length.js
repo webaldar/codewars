e8239e529e7ac4dbf2f05718a8e5a89ae9b80837
webaldar <webaldar@gmail.com>
2023-11-26 18:31:50 +0300

    example

---
+++ 8kyu/add-length.js
@@ -0,0 +1,16 @@
+// 📌 DESCRIPTION:
+// Your task is to write a function that takes a String and 
+// returns an Array/list with the length of each word added to 
+// each element .
+// Example(Input --> Output)
+
+// "apple ban" --> ["apple 5", "ban 3"]
+// "you will win" -->["you 3", "will 4", "win 3"]
+
+// 💚 SOLUTION
+
+function addLength(str) {
+    
+    }
+  addLength('apple ban');
+  addLength('you will winn');
