be00790ac1cee58d0cd27f23ac50990181a67781
webaldar <webaldar@gmail.com>
2023-11-27 21:19:07 +0300

    example

---
+++ 8kyu/string-templates-bug-fixing-5.js
@@ -0,0 +1,10 @@
+// 📌 DESCRIPTION:
+// Oh no! Timmy hasn't followed instructions very carefully and 
+// forgot how to use the new String Template feature, Help Timmy 
+// with his string template so it works as he expects!
+
+// 💚 SOLUTION
+
+function buildString(...template){
+    return `I like ${template.join(', ')}!`;
+  });
