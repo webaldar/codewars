98beba7d8b92dbd852b0c79f1c1ca5747ed17ec3
webaldar <webaldar@gmail.com>
2023-12-01 17:33:38 +0300

    example

---
+++ 8kyu/sum-of-positive.js
@@ -0,0 +1,20 @@
+// 📌 DESCRIPTION:
+// You get an array of numbers, return the sum of all of the 
+// positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+// 💚 SOLUTION
+
+function positiveSum(arr) {
+  let sum = 0;
+  for(let el of arr) {
+    if(el > 0){
+        sum += el;
+    }
+  }
+  return sum;
+}
+
+console.log(positiveSum([1,-4,7,12]));
